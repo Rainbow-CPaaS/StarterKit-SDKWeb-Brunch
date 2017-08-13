9f2052430a89bcc207b4bca34e9674710d55736f
Olivier Anguenot <olivier.anguenot@al-enterprise.com>
2017-08-13 11:10:51 +0200

    First commit

---
+++ app/initialize.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // do your setup here
+  console.log("[DEMO] :: Rainbow Application started!");
+  
+    var applicationID = "", 
+        applicationSecret = "";
+
+    /* Bootstrap the SDK */
+    angular.bootstrap(document, ["sdk"]).get("rainbowSDK");
+
+    /* Callback for handling the event 'RAINBOW_ONREADY' */
+    var onReady = function onReady() {
+        console.log("[DEMO] :: On SDK Ready !");
+        // do something when the SDK is ready
+    };
+
+    /* Callback for handling the event 'RAINBOW_ONCONNECTIONSTATECHANGED' */
+    var onLoaded = function onLoaded() {
+        console.log("[DEMO] :: On SDK Loaded !");
+
+        rainbowSDK.initialize(applicationID, applicationSecret).then(function() {
+            console.log("[DEMO] :: Rainbow SDK is initialized!");
+        }).catch(function(err) {
+            console.log("[DEMO] :: Something went wrong with the SDK...", err);
+        });
+    };
+
+    /* Listen to the SDK event RAINBOW_ONREADY */
+    $(document).on(rainbowSDK.RAINBOW_ONREADY, onReady);
+
+    /* Listen to the SDK event RAINBOW_ONLOADED */
+    $(document).on(rainbowSDK.RAINBOW_ONLOADED, onLoaded);
+
+    /* Load the SDK */
+    rainbowSDK.load();
+});
